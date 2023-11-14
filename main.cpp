7bf2f40c60c8e45eb8c0ddd22f2418a8c8cb7095
xiloyye <115136750+xiloyye@users.noreply.github.com>
2023-11-14 17:07:45 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+#include "infection.h"
+
+
+int main()
+{
+    DISK_GEOMETRY pdg;
+    bool bResult;                 // generic results flag
+    bResult = GetDriveMsg(&pdg, 0);
+    bool res = GetPEMsg();
+}
